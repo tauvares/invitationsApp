f1e7fc617491a6924c9edaecb539c8a09e5f274b
tauvares <tauvares@gmail.com>
2017-01-23 19:59:02 -0200

    Testing sendgrid

--- common/models/guest.js
+++ common/models/guest.js
@@ -42,6 +42,7 @@ module.exports = function(Guest) {
     mail = new helper.Mail(from_email, subject, to_email, content)
 
     var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    console.log(process.env.SENDGRID_API_KEY);
     var request = sg.emptyRequest({
       method: 'POST',
       path: '/v3/mail/send',
