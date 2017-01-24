68521b7dda182a7dfdbf4ad8d4d7f7d848fe4bf4
tauvares <tauvares@gmail.com>
2017-01-24 15:00:13 -0200

    Sending email using a html template with images

--- common/models/guest.js
+++ common/models/guest.js
@@ -13,7 +13,7 @@ module.exports = function(Guest) {
     '  padding-right: 0 !important;'+
     '}</style>'+
     '<H2 align="center" style="color:red">InvitationsApp</H2>'+
-    '<img align="center" src="images/invitationsAppLogoMini.png">InvitationsApp</H2>'+
+    '<img align="center" src="https://invitationsapp.herokuapp.com/api/containers/images/download/invitationsAppLogoMini.png">'+
     '<HR>'+
     '<H3 align="center">' + req.eventname + '</H3>'+
     '<p align="center">Prezado responsável pela instituição - ' + req.guestname + ', </p>'+
