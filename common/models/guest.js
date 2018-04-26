1e57903282418bef085a47636f0c95952ce2c7e9
João Tavares Filho Segundo <joao@joao.com>
2018-04-26 20:07:53 -0300

    Update e-mail server to gmail

--- common/models/guest.js
+++ common/models/guest.js
@@ -106,7 +106,7 @@ module.exports = function (Guest) {
       // to:   credentials.user,                  // Send to yourself
       // you also may set array of recipients:
       // [ 'user1@gmail.com', 'user2@gmail.com' ]
-      from:    'tauvares@gmail.com',            // from: by default equals to user
+      from: 'ellencarolinej@gmail.com',            // from: by default equals to user
       // replyTo: credentials.user,            // replyTo: by default undefined
       // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
       subject: 'test subject',
